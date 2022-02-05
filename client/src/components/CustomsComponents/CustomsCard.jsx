bd6e8cc8e77937bef6bf962329b476d1ae56fb1b
OmerPerez <omerperez222@gmail.com>
2022-02-05 17:36:18 +0200

    custom page

--- client/src/components/CustomsComponents/CustomsCard.jsx
+++ client/src/components/CustomsComponents/CustomsCard.jsx
@@ -1,8 +1,33 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import React from "react";
 
 export default function CustomsCard({name, link, image}) {
   return (
-    <div className="car-card-div">
+    <div style={{ width: "40%" }}>
+      <Card style={{ margin: "10px", border: "solid 1px #363636" }}>
+        <a href={link} className="cancel-underline">
+          <img
+            src={image}
+            style={{
+              backgroundSize: "cover",
+              width: "100%",
+              height: "200px",
+              borderBottom: "solid 1px #363636",
+            }}
+          />
+        </a>
+        <CardContent
+          style={{
+            borderTop: "solid 1px #e2a021",
+            background: "#363636",
+            color: "#e2a021",
+          }}
+        >
+          <Typography gutterBottom variant="h4" component="div">
+            {name}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- client/src/components/Pages/Customs.jsx
+++ client/src/components/Pages/Customs.jsx
@@ -1,35 +1,40 @@
 import React from "react";
+import CustomsCard from "../CustomsComponents/CustomsCard";
 import PageTitle from "../Layout/PageTitle";
 
 export default function Customs() {
   return (
     <>
       <PageTitle page={"Customs Broker"} />
-      <div className="justify-content-center d-flex m-4">
-        <iframe
-          className="col-6"
-          src="https://randg.co.il/?gclid=Cj0KCQiAi9mPBhCJARIsAHchl1yMzOlP0RT74c79CgRdrxqiwioWQt1X9mhf0jql5TsUJ9WhMsiA7XQaAgNyEALw_wcB"
-        ></iframe>
-        <iframe
-          className="col-6"
-          height={"100%"}
-          width={"100%"}
-          src="https://randg.co.il/?gclid=Cj0KCQiAi9mPBhCJARIsAHchl1yMzOlP0RT74c79CgRdrxqiwioWQt1X9mhf0jql5TsUJ9WhMsiA7XQaAgNyEALw_wcB"
-        ></iframe>
+      <div className="d-flex justify-content-center mt-3">
+        <CustomsCard
+          name={"R.G.A"}
+          image={"/rga.jpeg"}
+          link={
+            "https://randg.co.il/?gclid=Cj0KCQiA3fiPBhCCARIsAFQ8QzW-Cy52nC_3KZF4ppHCXGQ-5IZH66ffDhJFZeNrVPiU5ef65swarYYaAsbPEALw_wcB"
+          }
+        />
+        <CustomsCard
+          name={"Gaash"}
+          image={"/gash.jpeg"}
+          link={"https://gaash-customs.co.il/customs-clearance/"}
+        />
       </div>
-      <div className="justify-content-center d-flex m-4">
-        <iframe
-          className="col-6"
-          height={"100%"}
-          width={"100%"}
-          src="https://randg.co.il/?gclid=Cj0KCQiAi9mPBhCJARIsAHchl1yMzOlP0RT74c79CgRdrxqiwioWQt1X9mhf0jql5TsUJ9WhMsiA7XQaAgNyEALw_wcB"
-        ></iframe>
-        <iframe
-          className="col-6"
-          height={"100%"}
-          width={"100%"}
-          src="https://randg.co.il/?gclid=Cj0KCQiAi9mPBhCJARIsAHchl1yMzOlP0RT74c79CgRdrxqiwioWQt1X9mhf0jql5TsUJ9WhMsiA7XQaAgNyEALw_wcB"
-        ></iframe>
+      <div className="d-flex justify-content-center">
+        <CustomsCard
+          name={"Orian"}
+          image={"/orian.jpeg"}
+          link={
+            "https://www.orian.com/article/%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99-%D7%90%D7%95%D7%A8%D7%99%D7%99%D7%9F/%D7%A2%D7%9E%D7%99%D7%9C%D7%95%D7%AA-%D7%9E%D7%9B%D7%A1-09-12-2019/"
+          }
+        />
+        <CustomsCard
+          name={"Noach - Niv"}
+          image={"/niv.jpeg"}
+          link={
+            "https://www.orian.com/article/%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99-%D7%90%D7%95%D7%A8%D7%99%D7%99%D7%9F/%D7%A2%D7%9E%D7%99%D7%9C%D7%95%D7%AA-%D7%9E%D7%9B%D7%A1-09-12-2019/"
+          }
+        />
       </div>
     </>
     //
