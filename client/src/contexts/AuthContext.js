abb06b4d46e1f6bf1cb8701d2e5f61c77f3232cc
Tamir Amar <tamir.amar@zerto.com>
2022-03-02 23:22:47 +0200

    sending new car to server

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -41,6 +41,7 @@ export default function AuthProvider({ children }) {
       .catch(function (error) {
         console.log(error);
       });
+
   }
 
   function login(email, password) {
@@ -113,78 +114,60 @@ export default function AuthProvider({ children }) {
     });
   }
 
-  function createNewCar(
-    company,
-    model,
-    year,
-    numberOfVehicleOwners,
-    engine,
-    km,
-    price,
-    fuel,
-    numberOfSeats,
-    doorCount,
-    gearbox,
-    firstRegistrationDate,
-    colour,
-    condition,
-    interiorDesign,
-    images
-  ){
+  function createNewCar (carObj){
     
-    const car = {
-      companyEnglish: company.english,
-      companyHebrew: company.hebrew,
-      model: model,
-      year: year,
-      numberOfVehicleOwners: numberOfVehicleOwners,
-      engine: engine,
-      km: km,
-      price: price,
-      netPrice: price * 0.7,
-      images: images,
-      fuelConsumption: fuel,
-      numberOfSeats: numberOfSeats,
-      doorCount: doorCount,
-      gearbox: gearbox,
-      emissionClass: "Euro6",
-      firstRegistration: firstRegistrationDate,
-      colour: colour,
-      condition: condition,
-      dealer: currentUser._id,
-    };
-    // const carData = new FormData();
-    // carData.append("companyEnglish", company.english);
-    // carData.append("companyHebrew", company.hebrew);
-    // carData.append("model", model);
-    // carData.append("year", year);
-    // carData.append("numberOfVehicleOwners", numberOfVehicleOwners);
-    // carData.append("engine", engine);
-    // carData.append("km", km);
-    // carData.append("price", price);
-    // carData.append("netPrice", price * 0.7);
-    // carData.append("dateForImages", date);
-    // carData.append("images", [...imagesNames]);
-    // carData.append("fuelConsumption", fuel);
-    // carData.append("numberOfSeats", numberOfSeats);
-    // carData.append("doorCount", doorCount);
-    // carData.append("gearbox", gearbox);
-    // carData.append("emissionClass", "Euro6");
-    // carData.append("firstRegistration", firstRegistrationDate);
-    // carData.append("colour", colour);
-    // carData.append("condition", condition);
-    // carData.append("iteriorDesign", interiorDesign);
-    // carData.append("dealer", Object(currentUser._id));
+    // const car = {
+    //   companyEnglish: carObj.company.english,
+    //   companyHebrew: carObj.company.hebrew,
+    //   model: carObj.model,
+    //   year: carObj.year,
+    //   numberOfVehicleOwners: carObj.numberOfVehicleOwners,
+    //   engine: carObj.engine,
+    //   km: carObj.km,
+    //   price: carObj.price,
+    //   netPrice: carObj.price * 0.7,
+    //   images: carObj.images,
+    //   fuelConsumption: carObj.fuel,
+    //   numberOfSeats: carObj.numberOfSeats,
+    //   doorCount: carObj.doorCount,
+    //   gearbox: carObj.gearbox,
+    //   emissionClass: "Euro6",
+    //   firstRegistration: carObj.firstRegistrationDate,
+    //   colour: carObj.colour,
+    //   condition: carObj.condition,
+    //   dealer: currentUser._id,
+    // };
+
+    const carData = new FormData();
+    carData.append("companyEnglish", carObj.company.english);
+    carData.append("companyHebrew", carObj.company.hebrew);
+    carData.append("model", carObj.model);
+    carData.append("year", carObj.year);
+    carData.append("numberOfVehicleOwners", carObj.numberOfVehicleOwners);
+    carData.append("engine", carObj.engine);
+    carData.append("km", carObj.km);
+    carData.append("price", carObj.price);
+    carData.append("netPrice", carObj.price * 0.7);
+    carData.append("dateForImages", carObj.date);
+    carData.append("images",carObj.images);
+    carData.append("fuelConsumption", carObj.fuel);
+    carData.append("numberOfSeats", carObj.numberOfSeats);
+    carData.append("doorCount", carObj.doorCount);
+    carData.append("gearbox", carObj.gearbox);
+    carData.append("emissionClass", "Euro6");
+    carData.append("firstRegistration", carObj.firstRegistrationDate);
+    carData.append("colour", carObj.colour);
+    carData.append("condition", carObj.condition);
+    carData.append("iteriorDesign", carObj.interiorDesign);
+    carData.append("dealer", Object(currentUser._id));
+    console.log(carData);
 
     return api
-      .post("/car/create", car)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-      
+    .post("/car/create", carData)
+    .then(function (response) {})
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   useEffect(() => {
