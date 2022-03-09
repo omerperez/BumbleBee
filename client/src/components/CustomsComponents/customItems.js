dcf2c8d34b1515e45b839174b949a71881f3ad6b
OmerPerez <linoylbekker@gmail.com>
2022-03-10 01:33:49 +0200

    create new file for static list of our customs services in custom page

---
+++ client/src/components/CustomsComponents/customItems.js
@@ -0,0 +1,26 @@
+export const costums = [
+  {
+    name: "R.G.A",
+    image: "/rga.jpeg",
+    link: "https://randg.co.il/?gclid=Cj0KCQiA3fiPBhCCARIsAFQ8QzW-Cy52nC_3KZF4ppHCXGQ-5IZH66ffDhJFZeNrVPiU5ef65swarYYaAsbPEALw_wcB",
+    color: "#6495ED",
+  },
+  {
+    name: "Gaash",
+    image: "/gash.jpeg",
+    link: "https://gaash-customs.co.il/customs-clearance/",
+    color: "#5F9EA0",
+  },
+  {
+    name: "Orian",
+    image: "/orian.jpeg",
+    link: "https://www.orian.com/article/%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99-%D7%90%D7%95%D7%A8%D7%99%D7%99%D7%9F/%D7%A2%D7%9E%D7%99%D7%9C%D7%95%D7%AA-%D7%9E%D7%9B%D7%A1-09-12-2019/",
+    color: "#483D8B",
+  },
+  {
+    name: "Noach - Niv",
+    image: "/niv.jpeg",
+    link: "https://www.orian.com/article/%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99-%D7%90%D7%95%D7%A8%D7%99%D7%99%D7%9F/%D7%A2%D7%9E%D7%99%D7%9C%D7%95%D7%AA-%D7%9E%D7%9B%D7%A1-09-12-2019/",
+    color: "#F08080",
+  },
+];
