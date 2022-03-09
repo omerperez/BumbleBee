f377be34ef5c0c86ffdfc99a3b23dfec133cb7da
linoybekker <linoylbekker@gmail.com>
2022-03-09 23:31:04 +0200

    Bugfix

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,31 +1,3 @@
-const express = require("express");
-const db = require("./db/index");
-const bodyParser = require("body-parser");
-const user = require("./Routes/userRoutes");
-const car = require("./Routes/carRoutes");
-const dotenv = require("dotenv");
-
-const cors = require("cors");
-const app = express();
-app.use(cors());
-dotenv.config();
-
-db.on("error", (error) => {
-  console.log(error);
-});
-
-app.use(bodyParser.urlencoded({ extended: true, useUnifiedTopology: true }));
-app.use(bodyParser.json());
-
-app.use("/user", user);
-app.use("/car", car);
-
-// app.use(express.static('front'));
-// app.get('s3Url', (req, res) => {
-
-// })
-
-app.listen(process.env.PORT);
 import React, { useContext, useState, useEffect, createContext } from "react";
 import { auth } from "../AuthFirebase/firebase";
 import axios from "axios";
