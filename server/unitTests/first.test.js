efbb172f76ae7fc5a64c370cf699b6430127696f
Tamir Amar <tamir.amar@zerto.com>
2022-04-21 15:15:26 +0300

    login test

--- server/unitTests/first.test.js
+++ server/unitTests/first.test.js
@@ -17,6 +17,7 @@ describe('Testing user API', () => {
         const response = await request(app).get('/user')
         expect(response.statusCode).toEqual(200)
     })
+
     test('add new user', async () => {
         const response = await request(app).post('/user/register').send({
             'email' : userEmail,
@@ -26,5 +27,16 @@ describe('Testing user API', () => {
         const newUser = response.body.newUser
         expect(newUser.email).toEqual(userEmail)
         expect(newUser.password).toEqual(userPassword)
+    })
+
+    test('login user', async () => {
+        const response = await request(app).post('/user/login').send({
+            'email' : userEmail,
+            'password' : userPassword
+        })
+        expect(response.statusCode).toEqual(200)
+        const User = response.body
+        expect(User.email).toEqual(userEmail)
+        expect(User.password).toEqual(userPassword)
     })
 })
