189bf1a3d92689bb0911028ed1762e186f5e5694
Tamir Amar <tamir.amar@zerto.com>
2022-04-21 13:50:45 +0300

    adding test file

---
+++ server/unitTests/first.tests.js
@@ -0,0 +1,17 @@
+const app = require('../app')
+const request = require('supertest')
+
+beforeAll(done => {
+    done()
+})
+
+afterAll(done => {
+    done()
+})
+
+describe('First testing', () => {
+    test('post get', async () => {
+        response = await request(app).get('/')
+        expect(response.statusCode).toEqual(200)
+    })
+})
