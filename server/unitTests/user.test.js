60c7d7a93867e35e5c80b7c87525667f8035cbba
Tamir Amar <tamir.amar@zerto.com>
2022-04-24 21:57:23 +0300

    working on unit tests

--- server/unitTests/user.test.js
+++ server/unitTests/user.test.js
@@ -18,25 +18,25 @@ describe('Testing user API', () => {
         expect(response.statusCode).toEqual(200)
     })
 
-    test('add new user', async () => {
-        const response = await request(app).post('/user/register').send({
-            'email' : userEmail,
-            'password' : userPassword
-        })
-        expect(response.statusCode).toEqual(200)
-        const newUser = response.body.newUser
-        expect(newUser.email).toEqual(userEmail)
-        expect(newUser.password).toEqual(userPassword)
-    })
+    // test('add new user', async () => {
+    //     const response = await request(app).post('/user/register').send({
+    //         'email' : userEmail,
+    //         'password' : userPassword
+    //     })
+    //     expect(response.statusCode).toEqual(200)
+    //     const newUser = response.body.newUser
+    //     expect(newUser.email).toEqual(userEmail)
+    //     expect(newUser.password).toEqual(userPassword)
+    // })
 
-    test('login user', async () => {
-        const response = await request(app).post('/user/login').send({
-            'email' : userEmail,
-            'password' : userPassword
-        })
-        expect(response.statusCode).toEqual(200)
-        const User = response.body
-        expect(User.email).toEqual(userEmail)
-        expect(User.password).toEqual(userPassword)
-    })
+    // test('login user', async () => {
+    //     const response = await request(app).post('/user/login').send({
+    //         'email' : userEmail,
+    //         'password' : userPassword
+    //     })
+    //     expect(response.statusCode).toEqual(200)
+    //     const User = response.body
+    //     expect(User.email).toEqual(userEmail)
+    //     expect(User.password).toEqual(userPassword)
+    // })
 })
