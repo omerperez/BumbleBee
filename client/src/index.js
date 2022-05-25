58ff9f88965d8b577573a6989b113b659fa7b819
Omer <omerperez222@gmail.com>
2022-05-25 15:39:05 +0300

    no message

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/styles/index.css";
-import { CookiesProvider } from "react-cookie";
 
 ReactDOM.render(
   <React.StrictMode>
