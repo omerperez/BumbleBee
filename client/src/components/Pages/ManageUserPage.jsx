f1af8a0fbc37514cd63594387190e43facd96b2a
linoybekker <linoylbekker@gmail.com>
2022-05-31 01:16:22 +0300

    cr remove imports that dont use

--- client/src/components/Pages/ManageUserPage.jsx
+++ client/src/components/Pages/ManageUserPage.jsx
@@ -1,4 +1,4 @@
-import React ,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import PageTitle from "../Layout/PageTitle";
 import Loading from "../Layout/Loading";
 import SmartTable from "../TableComponents/SmartTable";
