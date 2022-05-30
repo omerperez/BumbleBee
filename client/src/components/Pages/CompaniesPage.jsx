8d95b194c0a138e29aea3864750b763fc252291b
linoybekker <linoylbekker@gmail.com>
2022-05-31 01:15:50 +0300

    remove spaces and comments

--- client/src/components/Pages/CompaniesPage.jsx
+++ client/src/components/Pages/CompaniesPage.jsx
@@ -4,15 +4,12 @@ import { error403 } from "../images/projectImages";
 import PageTitle from "../Layout/PageTitle";
 
 export default function CompaniesPage() {
-
     const [cars, setCars ] = useState([]);
-
     useEffect(() => {
         fetch(`${process.env.REACT_APP_SERVER_API}/car/`)
           .then((response) => response.json())
           .then((data) => setCars(data));
     }, []);
-
     const companies = Array.from(new Set(cars.map((a) => a.company))).map(
       (company) => {
         return cars.find((a) => a.company === company);
