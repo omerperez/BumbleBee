b10833df434a4a3a609513d304b35ec04c552d3b
linoybekker <linoylbekker@gmail.com>
2022-05-31 01:01:21 +0300

    change rating component to support null value

--- client/src/components/ProfileComponents/RatingDealer.jsx
+++ client/src/components/ProfileComponents/RatingDealer.jsx
@@ -2,6 +2,9 @@ import React, {useState} from 'react';
 import Rating from '@mui/material/Rating';
 
 export default function RatingDealer({ readOnly, ratingCount, setRatingCount }) {
+  if(ratingCount === null){
+    return <h4 className='fw-100 mt-3'>No Rating Yet</h4>
+  }
   return (
     <Rating
       style={readOnly ? null : {fontSize: 60 }}
